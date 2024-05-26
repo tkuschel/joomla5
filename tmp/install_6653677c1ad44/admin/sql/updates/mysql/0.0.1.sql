9fd079a8c7ad94dbad8ec52cf149b18d5f3fea3a
Thomas Kuschel <thomas.kuschel@technikum-wien.at>
2024-05-26 20:57:23 +0200

    DEL /tmp UPD .gitignore

--- tmp/install_6653677c1ad44/admin/sql/updates/mysql/0.0.1.sql
+++
@@ -1,8 +0,0 @@
--- @package		Depot.SQL MariaDB -- UPDATE to 0.0.1
--- @subpackage	com_depot
--- @author		Thomas Kuschel <thomas@kuschel.at>
--- @copyright	(C) 2023 KW4NZ, <https://www.kuschel.at>
--- @license		GNU General Public License version 2 or later; see LICENSE.md
--- @since		0.0.2
-
--- dummy
--- tmp/install_6653677c1ad44/admin/sql/updates/mysql/0.9.12.sql
+++
@@ -1,9 +0,0 @@
--- @package		Depot.SQL MariaDB -- UPDATE to 0.9.12
--- @subpackage	com_depot
--- @author		Thomas Kuschel <thomas@kuschel.at>
--- @copyright	(C) 2023 KW4NZ, <https://www.kuschel.at>
--- @license		GNU General Public License version 2 or later; see LICENSE.md
--- @since		0.9
-ALTER TABLE `#__depot_package`
-ADD COLUMN `modified` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `checked_out_time`,
-ADD COLUMN `modified_by` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `modified`;
